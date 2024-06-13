517cf641d4f7eb5ee7a59160bb7dce205ae3b510
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-06-13 19:02:10 +0300

    fix deploy issue

--- src/pages/Form/ServiceForm.jsx
+++ src/pages/Form/ServiceForm.jsx
@@ -57,11 +57,11 @@ const ServiceForm = () => {
                 <textarea
                   id="message"
                   rows="4"
-                  class="w-full rounded-lg border-2 border-stroke bg-transparent py-2 px-4 text-black outline-none transition-all duration-200 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-200 dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                  className="w-full rounded-lg border-2 border-stroke bg-transparent py-2 px-4 text-black outline-none transition-all duration-200 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-200 dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                   placeholder="وصف الخدمة"
                 ></textarea>
               </div>
-              <div class="flex items-center justify-center w-full">
+              <div className="flex items-center justify-center w-full">
                 <label
                   htmlFor="dropzone-file"
                   className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
